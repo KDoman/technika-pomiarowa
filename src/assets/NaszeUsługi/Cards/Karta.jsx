c426395602cf4dc4f1c114bc6fa88dba4af2d44f
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2023-05-12 13:17:40 +0200

    update website with animation

--- src/assets/NaszeUsługi/Cards/Karta.jsx
+++ src/assets/NaszeUsługi/Cards/Karta.jsx
@@ -1,26 +1,34 @@
 import "./Karta.css";
+import { motion } from "framer-motion";
 
 export function Karta(props) {
   return (
-    <div className="karta-container">
-      <div className="karta-container-div">
-        <p>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ delay: 0.2, duration: 1.5 }}
+    >
+      <div className="karta-container">
+        <div className="karta-container-div">
+          <p>
+            <img
+              className="karta-img"
+              src="Logo.png"
+              alt="Logo technika pomiarowa"
+            ></img>
+            {props.title}
+          </p>
           <img
-            className="karta-img"
-            src="Logo.png"
-            alt="Logo technika pomiarowa"
+            className="karta-container-img"
+            src={props.src}
+            alt={props.alt}
           ></img>
-          {props.title}
-        </p>
-        <img
-          className="karta-container-img"
-          src={props.src}
-          alt={props.alt}
-        ></img>
+        </div>
+        <div className="karta-container-div">
+          <p className="karta-container-p">{props.p}</p>
+        </div>
       </div>
-      <div className="karta-container-div">
-        <p className="karta-container-p">{props.p}</p>
-      </div>
-    </div>
+    </motion.div>
   );
 }
--- src/assets/NavigationBar/Nav.jsx
+++ src/assets/NavigationBar/Nav.jsx
@@ -1,31 +1,35 @@
 import "./Nav.css";
+import { motion } from "framer-motion";
 
 export function Nav() {
   return (
     <nav className="nav-container">
-      <div className="nav-container_nav-bar">
-        <img src="Logo.png" className="nav-container_nav-bar-img"></img>
-        <ul className="nav-container_nav-bar-ul">
-          <li>
-            <a>O nas</a>
-          </li>
-          <li>
-            <a>Nasze usługi</a>
-          </li>
-          <li>
-            <a>Doświadczenie</a>
-          </li>
-          <li>
-            <a>Kontakt</a>
-          </li>
-        </ul>
-      </div>
       <section className="nav-container_under-nav-section">
         <div className="nav-container_under-nav-div">
-          <h1 className="nav-container_under-nav-div-h1">Technika pomiarowa</h1>
-          <p className="nav-container_under-nav-div-p">
-            Przeglądy obiektów budowlanych/ pomiary elektryczne/ instalatorstwo
-          </p>
+          <motion.div
+            initial={{ y: 55, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2 }}
+          >
+            <h1 className="nav-container_under-nav-div-h1">
+              Technika pomiarowa
+            </h1>
+            <img
+              src="Logo.png"
+              alt="Logo Techniki pomiarowej"
+              className="nav-container_img-next-to-h1"
+            ></img>
+          </motion.div>
+          <motion.div
+            initial={{ y: 55, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, delay: 1 }}
+          >
+            <p className="nav-container_under-nav-div-p">
+              Przeglądy obiektów budowlanych/ pomiary elektryczne/
+              instalatorstwo
+            </p>
+          </motion.div>
         </div>
       </section>
     </nav>
