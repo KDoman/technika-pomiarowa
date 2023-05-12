172322953008406d6a3ccec3add824ae98024e1f
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2023-05-12 19:29:38 +0200

    update footer

--- src/assets/Kontakt/Kontakt.jsx
+++ src/assets/Kontakt/Kontakt.jsx
@@ -22,24 +22,42 @@ export function Kontakt() {
           <img src="Logo.png" alt="Technika pomiarowa Logo"></img>
         </div>
         <div className="footer-div-info">
-          <ul>
-            <li>
-              <span className="footer-span">Adres</span>
-              <span className="footer-text">
+          <ul className="footer-ul-redirection">
+            <li className="footer-ul-redirection-li">
+              <a>O nas</a>
+            </li>
+            <li className="footer-ul-redirection-li">
+              <a>Nasze usługi</a>
+            </li>
+            <li className="footer-ul-redirection-li">
+              <a>Doświadczenie</a>
+            </li>
+            <li className="footer-ul-redirection-li">
+              <a>Kontakt</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-div-info">
+          <ul className="footer-company-info-ul">
+            <li className="footer-company-info-li">
+              <span className="footer-company-span">Adres</span>
+              <span className="footer-company-text">
                 Al. Jerozolimskie 85/21, Warszawa, 02-001
               </span>
             </li>
-            <li>
-              <span className="footer-span">Telefon</span>
-              <span className="footer-text">515-551-878</span>
+            <li className="footer-company-info-li">
+              <span className="footer-company-span">Telefon</span>
+              <span className="footer-company-text">515-551-878</span>
             </li>
-            <li>
-              <span className="footer-span">Mail</span>
-              <span className="footer-text">technika_pomiarowa@o2.pl</span>
+            <li className="footer-company-info-li">
+              <span className="footer-company-span">Mail</span>
+              <span className="footer-company-text">
+                technika_pomiarowa@o2.pl
+              </span>
             </li>
-            <li>
-              <span className="footer-span">NIP</span>
-              <span className="footer-text">9512185869</span>
+            <li className="footer-company-info-li">
+              <span className="footer-company-span">NIP</span>
+              <span className="footer-company-text">9512185869</span>
             </li>
           </ul>
         </div>
